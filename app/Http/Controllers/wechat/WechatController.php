358aea9c79377516e68b20dfeb72f7c1181ebec2
sunwei116 <583337529@qq.com>
2019-08-23 20:28:31 +0800

    表白

--- app/Http/Controllers/wechat/WechatController.php
+++ app/Http/Controllers/wechat/WechatController.php
@@ -484,32 +484,32 @@ class WechatController extends Controller
                 $info     = sprintf($template,$toUser,$fromUser,$time,$msgType,$content);
                 echo $info;
 
-            }
-        }elseif($postObj->Event == 'CLICK'){
-            //回复用户消息
-            \Log::info(121);
-           $data = DB::connection('mysql4')->table('biaobai')->where('openid',$postObj->FromUserName)->get();
-           $msg = '';
-           foreach ($data as $k => $v){
-               if ($v['status'] == 2){
-                   $v['userName'] = '匿名用户';
-               }
-               $msg .='收到'.$v['userName'].'的表白'.$v['content']."\n";
-           }
-            $toUser   = $postObj->FromUserName;
-            $fromUser = $postObj->ToUserName;
-            $time     = time();
-            $msgType  = 'text';
-            $content  = $msg;
-            $template = "<xml>
+            }elseif($postObj->Event == 'CLICK'){
+                //回复用户消息
+                \Log::info(121);
+                $data = DB::connection('mysql4')->table('biaobai')->where('openid',$postObj->FromUserName)->get();
+                $msg = '';
+                foreach ($data as $k => $v){
+                    if ($v['status'] == 2){
+                        $v['userName'] = '匿名用户';
+                    }
+                    $msg .='收到'.$v['userName'].'的表白'.$v['content']."\n";
+                }
+                $toUser   = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $time     = time();
+                $msgType  = 'text';
+                $content  = $msg;
+                $template = "<xml>
                               <ToUserName><![CDATA[%s]]></ToUserName>
                               <FromUserName><![CDATA[%s]]></FromUserName>
                               <CreateTime>%s</CreateTime>
                               <MsgType><![CDATA[%s]]></MsgType>
                               <Content><![CDATA[%s]]></Content>
                             </xml>";
-            $info     = sprintf($template,$toUser,$fromUser,$time,$msgType,$content);
-            echo $info;
+                $info     = sprintf($template,$toUser,$fromUser,$time,$msgType,$content);
+                echo $info;
+            }
         }
     }
 //获取临时二维码
