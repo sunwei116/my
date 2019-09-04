2e96f43bd868b8e0fcbdd309bc414d4146ac40d1
sunwei116 <583337529@qq.com>
2019-09-04 10:07:08 +0800

    aa

--- app/Http/Controllers/Api/MemberController.php
+++ app/Http/Controllers/Api/MemberController.php
@@ -11,6 +11,13 @@ class MemberController extends Controller
     //查询数据库
     public function lists()
     {
+        // 制定允许其他域名访问
+        header("Access-Control-Allow-Origin:*");
+        // 响应类型
+        header('Access-Control-Allow-Methods:POST');
+        // 响应头设置
+        header('Access-Control-Allow-Headers:x-requested-with, content-type');
+        // 要求传递的 name hobby以及 model
         $data = Member::get()->toArray();
         return json_encode($data,JSON_UNESCAPED_UNICODE);
 
--- resources/views/fuxi/lists.blade.php
+++ resources/views/fuxi/lists.blade.php
@@ -6,24 +6,35 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="/static/jquery.js"></script>
     <title>列表</title>
 </head>
 <body>
-<form action="{{url('lists')}}" method="get">
-    姓名 <input type="text" name="search" id="" value="{{$search}}">
-    <input type="submit" name="" id="" value="搜索">
-</form>
     <table border="`">
         <tr>
             <th>姓名</th>
             <th>年龄</th>
-            <th>性别</th>
-            <th>班级</th>
-            <th>操作</th>
         </tr>
        <tbody id="lists">
 
        </tbody>
     </table>
 </body>
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    $.ajax({
+        url:"http://www.wei678.top/api/member/lists",
+        dataType:'json',
+        success:function (res){
+            var tr = $("<tr></tr>");
+            tr.parent("<td>res.name</td>");
+            tr.parent("<td>res.age</td>");
+            $('#lists').append(tr);
+        },
+    })
+</script>
 </html>
