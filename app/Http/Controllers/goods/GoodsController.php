b90ba521c4d871bb8a1dfaca9c6a06f14d04641c
sunwei116 <583337529@qq.com>
2019-09-09 10:45:28 +0800

    aa

--- app/Http/Controllers/goods/GoodsController.php
+++ app/Http/Controllers/goods/GoodsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\goods;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
-use App\model\Goods;
+use App\Model\Goods;
 
 class GoodsController extends Controller
 {
