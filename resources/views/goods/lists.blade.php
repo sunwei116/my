0accc278290f8d3f8f02515f1e08f9d6fcc8d11a
sunwei116 <583337529@qq.com>
2019-09-09 14:25:55 +0800

    aa

--- resources/views/goods/lists.blade.php
+++ resources/views/goods/lists.blade.php
@@ -25,14 +25,40 @@
                     tr.append("<td>"+v.goods_price+"</td>");
                     $('#tbody').append(tr);
                 });
+                var page = '';
                 for (var i=1;i<=res.data.last_page;i++){
                     if (i == res.data.current_page) {
-                        $('.pagination').append("<li class='active'><a href='javascript:;'>第"+i+"页</a></li>");
+                        page += "<li class='active'><a href='javascript:;' page='+i+'>第"+i+"页</a></li>";
                     }else{
-                        $('.pagination').append("<li><a href='javascript:;'>第"+i+"页</a></li>");
+                       page += "<li class='active'><a href='javascript:;' page='+i+'>第"+i+"页</a></li>"
                     }
                 }
             },
+        });
+        $(document).on('click','.pagination a',function () {
+            var page = $(this).attr('page');
+            $.ajax({
+                url:"http://www.wei678.top/api/goods/lists",
+                data:{page:page},
+                type:'POST',
+                dataType: 'json',
+                success:function (res) {
+                    $('#tbody').empty();
+                    $.each(res.data.data,function (i,v) {
+                        var tr = $("<tr></tr>");
+                        tr.append("<td>"+v.goods_name+"</td>");
+                        tr.append("<td>"+v.goods_price+"</td>");
+                        $('#tbody').append(tr);
+                    });
+                    for (var i=1;i<=res.data.last_page;i++){
+                        if (i == res.data.current_page) {
+                            page += "<li class='active'><a href='javascript:;' page='+i+'>第"+i+"页</a></li>";
+                        }else{
+                            page += "<li class='active'><a href='javascript:;' page='+i+'>第"+i+"页</a></li>"
+                        }
+                    }
+                }
+            })
         });
     </script>
 @endsection
